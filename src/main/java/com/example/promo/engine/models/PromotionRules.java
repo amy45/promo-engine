20786aafa1df549b616bfb44db67d2975c68bf42
Amit Jethwani <amit.jethwani@siemens.com>
2020-10-04 17:43:06 +0530

    PromotionRule class to hold the promotions for SKU

---
+++ src/main/java/com/example/promo/engine/models/PromotionRules.java
@@ -0,0 +1,14 @@
+package com.example.promo.engine.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Amit Jethwani
+ */
+@Data
+@AllArgsConstructor
+public class PromotionRules {
+    int units;
+    long price;
+}
