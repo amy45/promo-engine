0ca33ccce908726c1d0cf8a2d9a84a8e7f94b328
Amit Jethwani <amit.jethwani@siemens.com>
2020-10-04 17:41:18 +0530

    Static map for initial costs for SKU's

--- src/main/java/com/example/promo/engine/services/PromotionService.java
+++ src/main/java/com/example/promo/engine/services/PromotionService.java
@@ -2,11 +2,24 @@ package com.example.promo.engine.services;
 
 import com.example.promo.engine.enums.SKUTYPE;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Amit Jethwani
  */
 public class PromotionService {
+
+    private static Map<SKUTYPE, Integer> skuPriceMap = new HashMap<>();
+
+    static {
+        skuPriceMap.put(SKUTYPE.A, 50);
+        skuPriceMap.put(SKUTYPE.B, 30);
+        skuPriceMap.put(SKUTYPE.C, 20);
+        skuPriceMap.put(SKUTYPE.D, 15);
+    }
+
     public int getPriceByType(SKUTYPE c) {
-        return 0;
+        return skuPriceMap.getOrDefault(c, 0);
     }
 }
