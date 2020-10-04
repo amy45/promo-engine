9bd61d615fad1a8238ca7931bf26b8513dc6ee45
Amit Jethwani <amit.jethwani@siemens.com>
2020-10-04 17:31:45 +0530

    Sample test Case

---
+++ src/test/java/PromotionEngineTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Amit Jethwani
+ */
+public class PromotionEngineTest {
+
+
+
+    @Test
+    public void when_sku_is_A_should_return(){
+        Assert.assertEquals(50,skuService.getPriceByType(SKUTYPE.A));
+    }
+
+    @Test
+    public void when_sku_is_B_should_return(){
+        Assert.assertEquals(30,skuService.getPriceByType(SKUTYPE.B));
+    }
+
+    @Test
+    public void when_sku_is_C_should_return(){
+        Assert.assertEquals(20,skuService.getPriceByType(SKUTYPE.C));
+    }
+
+    @Test
+    public void when_sku_is_D_should_return(){
+        Assert.assertEquals(15,skuService.getPriceByType(SKUTYPE.D));
+    }
+}
